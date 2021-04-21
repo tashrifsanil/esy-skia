aa5be68fd1f6a5ad2b427d651e4c0c266d6a93ba
Zach Baylin <me@zachbayl.in>
2021-04-21 17:49:20 -0400

    Build: print tree (debug)

--- esy/build.sh
+++ esy/build.sh
@@ -20,8 +20,8 @@ if [[ $OS == "windows" ]]
 then
     WINDOWS_PYTHON_PATH="$(cygpath -w $(which $PYTHON_BINARY))"
     bin/gn gen $cur__target_dir/out/Shared --script-executable="$WINDOWS_PYTHON_PATH" --args='is_debug=false skia_enable_tools=true' || exit -1
-    cat $cur__target_dir/out/Shared/build.ninja
     ninja.exe -C $cur__target_dir/out/Shared skia skia.h experimental_svg_model
+    tree $cur__target_dir/out/Shared
     mv $cur__target_dir/out/Shared/libskia.dll $cur__target_dir/out/Shared/skia.dll # TODO this might not be required once we merge upstream
     esy/gendef.exe - $cur__target_dir/out/Shared/skia.dll > $cur__target_dir/out/Shared/skia.def
     x86_64-W64-mingw32-dlltool.exe -D $cur__target_dir/out/Shared/skia.dll -d $cur__target_dir/out/Shared/skia.def -A -l $cur__target_dir/out/Shared/libskia.a
