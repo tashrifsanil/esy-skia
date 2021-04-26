3194e39455151cc5aea3f6962a5cb4e9f3bec30c
Zach Baylin <me@zachbayl.in>
2021-04-26 10:29:16 -0700

    Build/Install: don't build separate SVG library on macOS

--- esy/build.sh
+++ esy/build.sh
@@ -41,9 +41,6 @@ else
         echo "llvm toolset-7.0 does not need to be manually activated"
     fi
 
-    bin/gn gen $cur__target_dir/out/Static --script-executable="$PYTHON_BINARY" "--args=cc=\"$CC\" cxx=\"$CXX\" skia_use_system_libjpeg_turbo=true skia_enable_tools=true is_debug=false extra_cflags=[\"-I${ESY_LIBJPEG_TURBO_PREFIX}/include\"] extra_ldflags=[\"-L${ESY_LIBJPEG_TURBO_PREFIX}/lib\", \"-ljpeg\" ]" || exit -1
-    ninja.exe -C $cur__target_dir/out/Static skia skia.h experimental_svg_model || exit -1
-    
-    # Create a new library from the svg object file
-    ar rcs $cur__target_dir/out/Static/libskiasvg.a $cur__target_dir/out/Static/obj/experimental/svg/model/*.o
+    bin/gn gen $cur__target_dir/out/Static --script-executable="$PYTHON_BINARY" "--args=cc=\"$CC\" cxx=\"$CXX\" skia_use_system_libjpeg_turbo=true esy_skia_enable_svg=true is_debug=false extra_cflags=[\"-I${ESY_LIBJPEG_TURBO_PREFIX}/include\"] extra_ldflags=[\"-L${ESY_LIBJPEG_TURBO_PREFIX}/lib\", \"-ljpeg\" ]" || exit -1
+    ninja.exe -C $cur__target_dir/out/Static || exit -1
 fi
